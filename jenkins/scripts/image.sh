a38d71191d846d1c39ad726b3a676b2a9265a78f
Ellipsis <114290672+rghr123@users.noreply.github.com>
2024-08-07 16:01:05 +0800

    Update image.sh

--- jenkins/scripts/image.sh
+++ jenkins/scripts/image.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
- 
+
 set -x
- 
-IMAGE_NAME="prometheus-test-demo"
+
+IMAGE_NAME="demo"
 VERSION_ID="${BUILD_ID}"
-IMAGE_ADDR="test-harbor.daocloud.io/library/${IMAGE_NAME}"
- 
+IMAGE_ADDR="harbor.edu.cn/nju12/${IMAGE_NAME}"
+
 docker build -f Dockerfile --build-arg jar_name=target/prometheus-test-demo-0.0.1-SNAPSHOT.jar -t ${IMAGE_NAME}:${VERSION_ID} .
- 
+
 docker tag  ${IMAGE_NAME}:${VERSION_ID}  ${IMAGE_ADDR}:${VERSION_ID}
-docker login --username=admin --password=Harbor12345 test-harbor.daocloud.io
+docker login --username=nju12 --password=cloudnju12 harbor.edu.cn
 docker push ${IMAGE_ADDR}:${VERSION_ID}
